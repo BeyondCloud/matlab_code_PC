f8ddbc6c141350c2897f15946f30e4662a2eb4d4
BeyondCloud <a1989796@yahoo.com.tw>
2017-08-19 13:48:38 +0800

    done freq shift

--- change_pitch/wav2fvec.m
+++ change_pitch/wav2fvec.m
@@ -5,18 +5,28 @@ infilename = 'a_c.wav';
 %sound(y,Fs);
 L = 4096;
 %L = 4096
+index = 1;
 for i =4096:4096:88200
-    Y(i) = fft(y(i-4095:i),L);
+    Y(:,index) = fft(y(i-4095:i),L);
+    index = index +1;
+end 
+sqr2_12 =  2^(5/12);
+for i =1:4096
+    Y_shift(i,:) = Y(ceil(i/sqr2_12),:);
 end
-%Y = fft(y,L);
+    %Y = fft(y,L);
 P2 = abs(Y/L);
-P1 = P2(1:L/2+1);
+P1 = P2(1:L/2+1,:);
 P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(L/2))/L;
 %figure
 %semilogx(f,P1)
 plot(f,P1) 
-axis([20 2000 0 0.15])
+axis([20 3000 0 0.15])
 title('Single-Sided Amplitude Spectrum of X(t)')
 xlabel('f (Hz)')
 ylabel('|P1(f)|')
+for i = 1:size(Y,2)
+    X(:,i) = ifft(Y_shift(:,i),L);
+end
+wavout = reshape(X,[4096*21,1]);
