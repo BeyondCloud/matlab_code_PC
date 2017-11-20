315ec50aa77e29ebc1b03842ade066df95ceaca8
BeyondCloud <a1989796@yahoo.com.tw>
2017-11-20 15:40:26 +0800

    add fix_pit

--- looping/looping2.m
+++ looping/looping2.m
@@ -1,5 +1,6 @@
 clear; clc; close all;
-[x,fs] = audioread('_a_ao.wav');
+[x,fs] = audioread('_a_e.wav');
+org=x;
 val=0;
 init_h_i = 60000
 init_t_i = 67000
@@ -20,12 +21,12 @@ lap = tail.*b +head.*a;
 mid = x(lap_len+1:x_len-lap_len+1);
 result = [lap;mid];
 
- result = repmat(result,8,1);
+ result = repmat(result,4,1);
 %  sound(result,44100);
-% hold on;
+ hold on;
 % plot(lap);
-%plot(x(x_len-1000:x_len).*b);
-%plot(x(1:1001).*a);
+plot(x(x_len-1000+1:x_len).*b);
+plot(x(1:1000).*a);
 %  plot(head);
 %  plot(tail);
  
