b08e72e5aa82ae6b39f37ee481fb6addc5ceef3a
BeyondCloud <a1989796@yahoo.com.tw>
2018-01-15 01:54:54 +0800

    done PIT

--- MATLAB_TSM-Toolbox_2.01/PIT.m
+++ MATLAB_TSM-Toolbox_2.01/PIT.m
@@ -3,16 +3,13 @@ start_f = 1.2;
 end_f = 2;
 del_f =end_f - start_f ;
 k_spline = 4;
-f_tbl = [start_f:del_f/(length(x)-1):end_f];
-stretch_ratio = sum(f_tbl)/(length(x)-1);
+x_len = length(x);
+f_tbl = [start_f:del_f/(x_len-2):end_f];
+f_tbl_cum = cumsum(f_tbl);
+stretch_ratio = sum(f_tbl)/(x_len-1);
 %%%stretching input x
 x_str = wsolaTSM(x,stretch_ratio);
-samp_pnt = f_tbl(1)+1;
-yy = zeros([1,44100]);
-for i = 2:44100
-    ana_start = max(1,ceil(samp_pnt-k_spline/2));
-    ana_end = min(length(x_str),floor(samp_pnt+k_spline/2));
-    ana_pnts = [ana_start:ana_end];
-    yy(i) = spline(ana_pnts,x_str(ana_pnts),samp_pnt);
-    samp_pnt=samp_pnt+f_tbl(i);
- end
+yy = zeros([1,x_len]);
+yy(1) = x_str(1);
+ana_pnts = [ana_start:ana_end];
+yy(2:end) = spline(1:length(x_str),x_str,f_tbl_cum);
