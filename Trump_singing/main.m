55b953a453c48f9b393c2c6de17733b20a7bd967
BeyondCloud <a1989796@yahoo.com.tw>
2018-06-10 18:05:51 +0800

    add trump

--- Trump_singing/main.m
+++ Trump_singing/main.m
@@ -4,10 +4,10 @@ addpath('E:\Matlab_code\MATLAB_TSM-Toolbox_2.01');
 ratio = (f0_pit/2^(0/12))./f0_lyc;
 % ratio  = ones(size(ratio));
 frame_size = 6000;
-
+x_len = length(x_talk);
 nframes = ceil(x_len/frame_size);
 x = [x_talk;zeros(nframes*frame_size-length(x_talk),1)];
-x_len = length(x);
+
 result = zeros(x_len,1);
 for i = 1:0.5:nframes-0.5
     range =( ((i-1)*frame_size+1):i*frame_size);
